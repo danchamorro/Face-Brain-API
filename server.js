2952308cc963812ea1df6116ec11cafb6d246cc9
Dan Chamorro <dan@danchamorro.com>
2019-01-30 11:12:11 -0500

    Finished sign in route and coonect to database.

--- server.js
+++ server.js
@@ -23,35 +23,56 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials ");
+      }
+    })
+    .catch(err => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
 
-  bcrypt.hash(password, null, null, function(err, hash) {
-    // Store hash in your password DB.
-    console.log(hash);
+  const hash = bcrypt.hashSync(password);
 
-    db("users")
-      .returning("*")
+  db.transaction(trx => {
+    trx
       .insert({
-        email: email,
-        name: name,
-        joined: new Date()
+        hash: hash,
+        email: email
       })
-      .then(user => {
-        res.json(user[0]);
-      })
-      .catch(err => {
-        res.status(400).json("unable to register");
+      .into("login")
+      .returning("email")
+      .then(loginEmail => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+          .then(trx.commit)
+          .catch(trx.rollback)
+          .catch(err => {
+            res.status(400).json("unable to register");
+          });
       });
   });
 });
