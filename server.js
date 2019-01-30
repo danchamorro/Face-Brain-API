f4762ce27bc82ad6cddd224a6454e906c105601f
Dan Chamorro <dan@danchamorro.com>
2019-01-30 18:25:42 -0500

    CHnage Port

--- server.js
+++ server.js
@@ -47,8 +47,10 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(5000, () => {
-  console.log("Example app listening on port 5000!");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 //Run app, then load http://localhost:5000 in a browser to see the output.
